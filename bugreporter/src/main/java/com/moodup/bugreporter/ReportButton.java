9c2a15e61f69cc365ed13087ca6e3df518b3a96c
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-05-11 11:42:02 +0200

    Add ReportButton class

---
+++ bugreporter/src/main/java/com/moodup/bugreporter/ReportButton.java
@@ -0,0 +1,19 @@
+package com.moodup.bugreporter;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class ReportButton extends MontserratTextView {
+
+    public ReportButton(Context context) {
+        super(context);
+    }
+
+    public ReportButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ReportButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
