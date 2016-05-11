fbcd3566af68298761faf6111676674963aeb642
bsmektal <bs.smektala@gmail.com>
2016-05-11 11:51:12 +0200

    Add methods to confirmation dialog

---
+++ bugreporter/src/main/java/com/moodup/bugreporter/ConfirmationDialog.java
@@ -0,0 +1,72 @@
+package com.moodup.bugreporter;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import butterknife.ButterKnife;
+
+public class ConfirmationDialog extends DialogFragment {
+    //region Consts
+    public static final int TYPE_FAILURE = 0;
+    public static final int TYPE_SUCCESS = 1;
+    //endregion
+    //region Fields
+    //endregion
+
+    //region Override Methods
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        CustomDialog dialog = new CustomDialog(getActivity(), R.style.CustomDialog);
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_confirmation, null);
+        ButterKnife.bind(this, v);
+        dialog.setContentView(v);
+
+        initViews(v);
+
+        return super.onCreateDialog(savedInstanceState);
+    }
+
+    //endregion
+
+    //region Events
+
+    //endregion
+
+    //region Methods
+    public static final ConfirmationDialog newInstance(int type) {
+        ConfirmationDialog dialog = new ConfirmationDialog();
+
+        Bundle bundle = new Bundle();
+        bundle.putInt("type", type);
+
+        dialog.setArguments(bundle);
+
+        return dialog;
+    }
+
+    private void initViews(View view) {
+        int type = getArguments().getInt("type", TYPE_SUCCESS);
+
+        ImageView icon = ButterKnife.findById(view, R.id.confirmation_dialog_icon);
+        MontserratTextView message = ButterKnife.findById(view, R.id.confirmation_dialog_message);
+        MontserratTextView okButton = ButterKnife.findById(view, R.id.confirmation_dialog_ok_button);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        icon.setRotation(type == TYPE_SUCCESS ? 0 : 180.0f);
+        message.setText(getString(type == TYPE_SUCCESS ? R.string.confirmation_success : R.string.confirmation_failure));
+    }
+    //endregion
+
+
+}
