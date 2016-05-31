c37a2711d871166c27fcf59cf1628358fa4513d2
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-05-31 09:12:14 +0200

    Refactor getting ApiClient

--- bugreporter/src/main/java/com/moodup/bugreporter/ApiClient.java
+++ bugreporter/src/main/java/com/moodup/bugreporter/ApiClient.java
@@ -1,6 +1,7 @@
 package com.moodup.bugreporter;
 
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.util.Base64;
 
 import java.io.BufferedWriter;
@@ -18,6 +19,19 @@ import java.util.Map;
 import javax.net.ssl.HttpsURLConnection;
 
 public class ApiClient {
+
+    public static final String TITLE = "title";
+    public static final String CONTENT = "content";
+    public static final String PRIORITY = "priority";
+    public static final String KIND = "kind";
+    public static final String GRANT_TYPE = "grant_type";
+    public static final String CLIENT_ID = "client_id";
+    public static final String SECRET = "secret";
+    public static final String REFRESH_TOKEN = "refresh_token";
+    public static final String AUTHORIZATION_CODE = "authorization_code";
+    public static final String CODE = "code";
+    public static final String METHOD_POST = "POST";
+    public static final String CHARSET_UTF8 = "UTF-8";
 
     private String repoSlug;
     private String accountName;
@@ -43,20 +57,20 @@ public class ApiClient {
 
     protected void addIssue(String title, String content, String priority, String kind, HttpHandler handler) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("title", title);
-        map.put("content", content);
-        map.put("priority", priority);
-        map.put("kind", kind);
+        map.put(TITLE, title);
+        map.put(CONTENT, content);
+        map.put(PRIORITY, priority);
+        map.put(KIND, kind);
 
         new AddIssueAsyncTask(map, handler).execute(String.format(BitBucket.ISSUES_URL, accountName, repoSlug));
     }
 
     protected void authorize(String token, String clientId, String clientSecret, boolean refresh, HttpHandler handler) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("grant_type", refresh ? "refresh_token" : "authorization_code");
-        map.put("client_id", clientId);
-        map.put("secret", clientSecret);
-        map.put(refresh ? "refresh_token" : "code", token);
+        map.put(GRANT_TYPE, refresh ? REFRESH_TOKEN : AUTHORIZATION_CODE);
+        map.put(CLIENT_ID, clientId);
+        map.put(SECRET, clientSecret);
+        map.put(refresh ? REFRESH_TOKEN : CODE, token);
 
         new ApiClient.AuthorizeAsyncTask(map, handler).execute(BitBucket.AUTHORIZE_URL);
     }
@@ -73,45 +87,7 @@ public class ApiClient {
 
         @Override
         protected HttpResponse doInBackground(String... params) {
-            URL url;
-            InputStream is;
-            try {
-                url = new URL(params[0]);
-
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(15000);
-                conn.setConnectTimeout(15000);
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-                conn.setRequestProperty("Authorization", "Basic " + Base64.encodeToString((postParams.get("client_id") + ":" + postParams.get("secret")).getBytes(), Base64.DEFAULT));
-                conn.addRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-
-                conn.setRequestMethod("POST");
-
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(getPostDataString(postParams));
-
-                writer.flush();
-                writer.close();
-                os.close();
-                int response = conn.getResponseCode();
-
-                if (response == HttpsURLConnection.HTTP_OK) {
-                    is = conn.getInputStream();
-                    return new HttpResponse(response, Utils.getStringFromInputStream(is));
-                } else if (response == HttpsURLConnection.HTTP_UNAUTHORIZED) {
-                    BugReporter.getInstance().authenticate(true);
-                    return new HttpResponse(response, "");
-                } else {
-                    return new HttpResponse(response, "");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return new HttpResponse();
+            return getHttpResponse(params, postParams, true);
         }
 
         @Override
@@ -133,52 +109,61 @@ public class ApiClient {
 
         @Override
         protected HttpResponse doInBackground(String... params) {
-            URL url;
-            InputStream is;
-            try {
-                url = new URL(params[0]);
-
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(15000);
-                conn.setConnectTimeout(15000);
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-                conn.addRequestProperty("Authorization", "Bearer " + URLDecoder.decode(accessToken, "UTF-8"));
-                conn.addRequestProperty("Content-Type", "application/x-www-form-urlencoded"); //add the content type of the request, most post data is of this type
-
-                conn.setRequestMethod("POST");
-
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(getPostDataString(postParams));
-
-                writer.flush();
-                writer.close();
-                os.close();
-                int response = conn.getResponseCode();
-
-                if (response == HttpsURLConnection.HTTP_OK) {
-                    is = conn.getInputStream();
-                    return new HttpResponse(response, Utils.getStringFromInputStream(is));
-                } else if (response == HttpsURLConnection.HTTP_UNAUTHORIZED) {
-                    BugReporter.getInstance().authenticate(true);
-                    return new HttpResponse(response, "");
-                } else {
-                    return new HttpResponse(response, "");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return new HttpResponse();
+            return getHttpResponse(params, postParams, false);
         }
+
 
         @Override
         protected void onPostExecute(HttpResponse result) {
             handler.done(result);
             super.onPostExecute(result);
         }
+    }
+
+    @NonNull
+    private HttpResponse getHttpResponse(String[] params, HashMap<String, String> postParams, boolean authorization) {
+        URL url;
+        InputStream is;
+        try {
+            url = new URL(params[0]);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.addRequestProperty("Authorization",
+                    authorization ?
+                            "Basic " + Base64.encodeToString((postParams.get(CLIENT_ID) + ":" + postParams.get(SECRET)).getBytes(), Base64.DEFAULT)
+                            : "Bearer " + URLDecoder.decode(accessToken, CHARSET_UTF8));
+            conn.addRequestProperty("Content-Type", "application/x-www-form-urlencoded"); //add the content type of the request, most post data is of this type
+
+            conn.setRequestMethod(METHOD_POST);
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, CHARSET_UTF8));
+            writer.write(getPostDataString(postParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+            int response = conn.getResponseCode();
+
+            if (response == HttpsURLConnection.HTTP_OK) {
+                is = conn.getInputStream();
+                return new HttpResponse(response, Utils.getStringFromInputStream(is));
+            } else if (response == HttpsURLConnection.HTTP_UNAUTHORIZED) {
+                BugReporter.getInstance().authenticate(true);
+                return new HttpResponse(response, "");
+            } else {
+                return new HttpResponse(response, "");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new HttpResponse();
     }
 
     private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
@@ -190,9 +175,9 @@ public class ApiClient {
             else
                 result.append("&");
 
-            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append(URLEncoder.encode(entry.getKey(), CHARSET_UTF8));
             result.append("=");
-            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+            result.append(URLEncoder.encode(entry.getValue(), CHARSET_UTF8));
         }
 
         return result.toString();
