ac7a71d7011055daf24c8d75858bd3feece38064
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-07-27 15:55:18 +0200

    Remove unused constant from BitBucket interface

--- bugreporter/src/main/java/com/moodup/bugreporter/BitBucket.java
+++ bugreporter/src/main/java/com/moodup/bugreporter/BitBucket.java
@@ -1,23 +1,25 @@
 package com.moodup.bugreporter;
 
 public interface BitBucket {
-    String REFERER_URL = "https://bitbucket.org";
-    String CALLBACK_URL = "http://callback.moodup.com";
-    String OAUTH_URL = "https://bitbucket.org/site/oauth2/authorize?client_id=%1$s&response_type=code";
+
+    // URLS
     String AUTHORIZE_URL = "https://bitbucket.org/site/oauth2/access_token";
     String ISSUES_URL = "https://api.bitbucket.org/1.0/repositories/%1$s/%2$s/issues";
+
     // PRIORITIES
     String PRIORITY_TRIVIAL = "trivial";
     String PRIORITY_MINOR = "minor";
     String PRIORITY_MAJOR = "major";
     String PRIORITY_CRITICAL = "critical";
     String PRIORITY_BLOCKER = "blocker";
+
     // ISSUE KINDS
     String KIND_BUG = "bug";
     String KIND_ENHANCEMENT = "enhancement";
     String KIND_PROPOSAL = "proposal";
     String KIND_TASK = "task";
 
+    // AUTH
     String GRANT_TYPE_PASSWORD = "password";
     String GRANT_TYPE_REFRESH_TOKEN = "refresh_token";
 }
