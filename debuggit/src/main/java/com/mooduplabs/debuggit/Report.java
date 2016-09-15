ef6340ee82d67d42e9acb27c292c42c116aef4c1
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-09-15 14:39:02 +0200

    Clear kind and priority after sending issue

--- debuggit/src/main/java/com/mooduplabs/debuggit/Report.java
+++ debuggit/src/main/java/com/mooduplabs/debuggit/Report.java
@@ -96,6 +96,8 @@ public class Report {
 
     protected void clear() {
         title = "";
+        kind = "";
+        priority = "";
         stepsToReproduce = "";
         actualBehaviour = "";
         expectedBehaviour = "";
