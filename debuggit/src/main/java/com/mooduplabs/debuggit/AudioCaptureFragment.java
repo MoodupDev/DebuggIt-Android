ef9f726bd5644e9509856d5b8ed782213e149fd3
Rafał Nowakiewicz <rafal.nowakiewicz@gmail.com>
2019-06-05 10:25:39 +0200

    Migrate debuggit and demo app to AndroidX

--- debuggit/src/main/java/com/mooduplabs/debuggit/AudioCaptureFragment.java
+++ debuggit/src/main/java/com/mooduplabs/debuggit/AudioCaptureFragment.java
@@ -2,10 +2,10 @@ package com.mooduplabs.debuggit;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
--- debuggit/src/main/java/com/mooduplabs/debuggit/WelcomeDialog.java
+++ debuggit/src/main/java/com/mooduplabs/debuggit/WelcomeDialog.java
@@ -2,7 +2,7 @@ package com.mooduplabs.debuggit;
 
 import android.app.Dialog;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import androidx.fragment.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 
