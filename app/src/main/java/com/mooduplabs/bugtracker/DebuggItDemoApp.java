3256ee72f2fad34c70abccacbd751f11450bd216
Rafał Nowakiewicz <rafal.nowakiewicz@gmail.com>
2019-06-26 08:57:19 +0200

    Comment additional configuration options

--- app/src/main/java/com/mooduplabs/bugtracker/DebuggItDemoApp.java
+++ app/src/main/java/com/mooduplabs/bugtracker/DebuggItDemoApp.java
@@ -2,12 +2,7 @@ package com.mooduplabs.bugtracker;
 
 import android.app.Application;
 
-import com.mooduplabs.debuggit.ApiInterface;
 import com.mooduplabs.debuggit.DebuggIt;
-import com.mooduplabs.debuggit.JsonResponseCallback;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class DebuggItDemoApp extends Application {
 
@@ -18,32 +13,32 @@ public class DebuggItDemoApp extends Application {
         DebuggIt.getInstance()
                 .setRecordingEnabled(true)
                 .configureDefaultApi("https://url-to-backend.com/api", "/debuggit/uploadImage", "/debuggit/uploadAudio")
-                .configureCustomApi(new ApiInterface() {
-                    @Override
-                    public void uploadImage(String imageData, String appId, JsonResponseCallback callback) {
-                        JSONObject response = new JSONObject();
-
-                        try {
-                            response.put("url", "https://via.placeholder.com/150C");
-                            callback.onSuccess(response);
-                        } catch (JSONException ex) {
-                            callback.onFailure(400, "Could not upload image");
-                        }
-                    }
-
-                    @Override
-                    public void uploadAudio(String audioData, String appId, JsonResponseCallback callback) {
-                        JSONObject response = new JSONObject();
-
-                        try {
-                            response.put("url", "");
-                            callback.onSuccess(response);
-                        } catch (JSONException ex) {
-                            callback.onFailure(400, "Could not upload audio");
-                        }
-                    }
-                })
-                .configureS3Bucket("bucketName", "accessKey", "secretKey", "eu-central-1")
+//                .configureCustomApi(new ApiInterface() {
+//                    @Override
+//                    public void uploadImage(String imageData, String appId, JsonResponseCallback callback) {
+//                        JSONObject response = new JSONObject();
+//
+//                        try {
+//                            response.put("url", "https://via.placeholder.com/150C");
+//                            callback.onSuccess(response);
+//                        } catch (JSONException ex) {
+//                            callback.onFailure(400, "Could not upload image");
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void uploadAudio(String audioData, String appId, JsonResponseCallback callback) {
+//                        JSONObject response = new JSONObject();
+//
+//                        try {
+//                            response.put("url", "");
+//                            callback.onSuccess(response);
+//                        } catch (JSONException ex) {
+//                            callback.onFailure(400, "Could not upload audio");
+//                        }
+//                    }
+//                })
+//                .configureS3Bucket("bucketName", "accessKey", "secretKey", "eu-central-1")
                 .configureBitbucket("repositoryName", "accountName")
                 .init();
     }
